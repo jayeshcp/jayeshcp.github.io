4b6c329cc826e678f2155715e006322407db1c27
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2026-03-06 10:26:44 -0500

    Add cache busting for assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 export default function (eleventyConfig) {
+  eleventyConfig.addFilter("bust", (url) => {
+    const stat = fs.statSync(`./_site${url}`);
+    const mtime = stat.mtimeMs;
+    return `${url}?v=${mtime}`;
+  });
+
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPlugin(EleventyVitePlugin, {
     viteOptions: {
