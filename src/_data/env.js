39c8a828ffa6544886480abacbf27e0da7562c01
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2026-03-09 12:55:06 -0400

    code cleanup

--- src/_data/env.js
+++ src/_data/env.js
@@ -3,9 +3,6 @@ const PROD_ENV = "prod";
 const isProd = environment === PROD_ENV;
 const GTAG = process.env.GTAG || "";
 
-console.log(`isProd value: ${isProd}`);
-console.log(`GTAG value: ${GTAG}`);
-
 export default {
   isProd,
   GTAG,
